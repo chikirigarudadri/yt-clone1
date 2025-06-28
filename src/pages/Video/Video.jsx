407172a715839592743554f618cf69d1922a2fbf
chikirigarudadri <149191409+chikirigarudadri@users.noreply.github.com>
2025-06-28 14:46:19 +0530

    Add files via upload

---
+++ src/pages/Video/Video.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Video.css'
+import RecommendVideo from '../../Components/RecommendVideo/RecommendVideo';
+import PlayVideo from '../../Components/Playvideo/Playvideo';
+import { useParams } from 'react-router-dom';
+const Video = () => {
+
+ const {videoId,categoryId} = useParams();
+  
+  return (
+    <div className="play-container">
+
+<PlayVideo videoId ={videoId}/>
+
+      <RecommendVideo categoryId={categoryId}/>
+
+    </div>
+  )
+}
+
+export default Video;
